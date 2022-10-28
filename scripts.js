502905e1a28bd7921d74f77d7734b61a94f93f4a
Bogdan Voskanov <bogdan.voskanov@platfoza.com>
2022-10-28 22:36:08 +0300

    done

--- scripts.js
+++ scripts.js
@@ -1,23 +1,35 @@
-async function lottery() {
-  console.log("Вы начали игру");
+const loadBtn = document.querySelector(".js-load");
+const resultsContainer = document.querySelector(".js-results");
+const searchInput = document.querySelector(".js-input");
 
-  try {
-    let promise = new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        Math.random(0) > 0.5 ? resolve("Победа") : reject("вы промахнулись");
-      }, 1000);
-    });
+loadBtn.addEventListener("click", function (evt) {
+  evt.preventDefault()
+  const searchValue = searchInput.value.trim().toLowerCase();
+  fetch(`https://api.github.com/users/${searchValue}`).then(
+    (res) => res.json()).then(data =>
+      resultsContainer.innerHTML =
+      `<div class="response-container">
+                <img src="${data.avatar_url}">
+                <p> Имя: <span>${data.name}</span><p>
+                <p> О себе: <span>${data.bio}</span><p>
+                <p> Кол-во репозиториев: <span>${data.public_repos}</span><p>
+            </div>`
+    ).finally(() => {
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5").then(({ data }) => {
+        const postsContainer = document.createElement('div');
+        postsContainer.classList.add('posts__container')
+        const posts = data.map(el => {
+          return (`
+          <div class="post">
+            <span class="post__id">Post #${el.id}</span>
+            <h3 class="post__title">${el.title}</h3>
+            <p class="post__body">${el.body}</p>
+          </div>
+          `)
+        })
+        postsContainer.innerHTML = posts.join('')
+        resultsContainer.append(postsContainer)
+      })
+    })
 
-    const result = await promise;
-
-    console.log(result);
-  } catch (error) {
-    console.log("К сожалению", error);
-  } finally {
-    console.log("Конечный код");
-  }
-}
-
-lottery();
-
-
+});
